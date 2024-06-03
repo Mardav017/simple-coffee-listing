bd48da9bac967f667ab68a3c49c5c6215919b23e
Mardav <mardav17jp@gmail.com>
2024-06-03 12:36:52 +0530

    Updated Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [showAvailable, setShowAvailable] = useState(false);
+
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
@@ -16,15 +18,23 @@ const Home = () => {
       });
   }, []);
 
+  const filteredData = showAvailable
+    ? data.filter((coffee) => coffee.available)
+    : data;
+
   return (
     <div className="homepage bg-[#111315] h-[1100px] font-sans">
-      <img src="./bg-cafe.jpg" alt="bg-cafe" className="w-full h-66 absolute" />
+      <img
+        src={`${process.env.PUBLIC_URL}/bg-cafe.jpg`}
+        alt="bg-cafe"
+        className="w-full h-66 absolute"
+      />
 
       <div className="collection bg-[#1B1D1F] h-fit w-[85%] mx-24 my-40 rounded-lg absolute">
         <div
           className="pt-10 mx-[280px] bg-contain bg-no-repeat bg-right mt-8"
           style={{
-            backgroundImage: "url('/vector.svg')",
+            backgroundImage: `url(${process.env.PUBLIC_URL}/vector.svg)`,
             backgroundSize: "50% 80%",
           }}
         >
@@ -37,13 +47,16 @@ const Home = () => {
           <button
             type="button"
             className="bg-[#6F757C] mt-5 text-white px-4 py-1.5 rounded-lg"
+            onClick={() => setShowAvailable(false)}
           >
             All Products
-          </button>{" "}
-          <span className="text-white ml-2">Available Now</span>
+          </button>
+          <button type="button" onClick={() => setShowAvailable(true)}>
+            <span className="text-white ml-2">Available Now</span>
+          </button>
         </div>
         <div className="grid grid-cols-3 gap-6 mx-24 mt-10">
-          {data.map((coffee) => (
+          {filteredData.map((coffee) => (
             <a
               href={`#${coffee.id}`}
               id={coffee.id}
@@ -72,22 +85,38 @@ const Home = () => {
                   {coffee.price}
                 </p>
               </div>
-              {coffee.rating ? (
-                <div className="flex text-white text-sm font-semibold">
-                  <img src="./Star_fill.svg" alt="star" />
-                  <span className="ml-1">
-                    {parseFloat(coffee.rating).toFixed(1)}
-                  </span>
-                  <span className="ml-1 text-[#6F757C]">
-                    ({coffee.votes} votes)
-                  </span>
-                </div>
-              ) : (
-                <div className="flex text-white text-sm font-semibold">
-                  <img src="./Star.svg" alt="star" />
-                  <span className="ml-1 text-[#6F757C]">No ratings</span>
-                </div>
-              )}
+
+              <div className="rating_available flex justify-between mt-3">
+                {coffee.rating ? (
+                  <div className="flex text-white text-sm font-semibold">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/Star_fill.svg`}
+                      alt="star"
+                    />
+                    <span className="ml-1">
+                      {parseFloat(coffee.rating).toFixed(2)}
+                    </span>
+                    <span className="ml-1 text-[#6F757C]">
+                      ({coffee.votes} votes)
+                    </span>
+                  </div>
+                ) : (
+                  <div className="flex text-white text-sm font-semibold">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/Star.svg`}
+                      alt="star"
+                    />
+                    <span className="ml-1 text-[#6F757C]">No ratings</span>
+                  </div>
+                )}
+                {coffee.available ? (
+                  ""
+                ) : (
+                  <p className="text-[#ED735D] font-semibold text-sm">
+                    Sold out
+                  </p>
+                )}
+              </div>
             </a>
           ))}
         </div>
